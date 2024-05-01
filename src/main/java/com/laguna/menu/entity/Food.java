565a43b07c20f15f39aff01cba3a80456e9e69c8
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-05-01 15:18:08 -0300

    getters and setters

--- src/main/java/com/laguna/menu/entity/Food.java
+++ src/main/java/com/laguna/menu/entity/Food.java
@@ -17,4 +17,36 @@ public class Food {
   private String name;
   private String imageUrl;
   private Integer price;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
+
+  public Integer getPrice() {
+    return price;
+  }
+
+  public void setPrice(Integer price) {
+    this.price = price;
+  }
 }
